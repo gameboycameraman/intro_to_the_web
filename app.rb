ffbacdf02221630cd1705c1364e128756fa2f231
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-28 11:12:00 +0100

    Adding Shotgun because badass

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello world!"
 end
+
+get "/secret" do
+  "I love crepes, because I'm awesome."
+end
+
+get "/crepes" do
+  "Told you, crepes are awesome !"
+end
+
+get "/VideoGames" do
+  "Need to play, like now !"
+end
